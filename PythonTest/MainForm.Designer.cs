a80ce95af4274c67bb6df5e730d00d0a6e130822
Kai Sun <autosunkai@gmail.com>
2020-12-03 20:11:45 +0800

    Initial repository

---
+++ PythonTest/MainForm.Designer.cs
@@ -0,0 +1,62 @@
+﻿namespace PythonTest
+{
+    partial class MainForm
+    {
+        /// <summary>
+        /// 必需的设计器变量。
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// 清理所有正在使用的资源。
+        /// </summary>
+        /// <param name="disposing">如果应释放托管资源，为 true；否则为 false。</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows 窗体设计器生成的代码
+
+        /// <summary>
+        /// 设计器支持所需的方法 - 不要修改
+        /// 使用代码编辑器修改此方法的内容。
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnRun = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // btnRun
+            // 
+            this.btnRun.Font = new System.Drawing.Font("宋体", 15.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(134)));
+            this.btnRun.Location = new System.Drawing.Point(313, 172);
+            this.btnRun.Name = "btnRun";
+            this.btnRun.Size = new System.Drawing.Size(160, 77);
+            this.btnRun.TabIndex = 0;
+            this.btnRun.Text = "RunPython";
+            this.btnRun.UseVisualStyleBackColor = true;
+            this.btnRun.Click += new System.EventHandler(this.btnRun_Click);
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRun);
+            this.Name = "MainForm";
+            this.Text = "MainForm";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnRun;
+    }
+}
+
---
+++ PythonTest/MainForm.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PythonTest
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+        }
+
+        public static void RunPythonScript(string sArguments)
+        {
+            Process p = new Process();
+            p.StartInfo.FileName = @"PythonEnvs\python27\python.exe";  // Python 的安装路径
+            p.StartInfo.Arguments = sArguments;  // Python 命令的参数
+            p.StartInfo.UseShellExecute = true;
+            p.StartInfo.RedirectStandardOutput = false;
+            p.StartInfo.RedirectStandardInput = false;
+            p.StartInfo.RedirectStandardError = false;
+            p.StartInfo.CreateNoWindow = false;
+            p.Start();  // 启动进程
+        }
+
+        private void btnRun_Click(object sender, EventArgs e)
+        {
+            //string sArguments = @"PythonScripts\pyMAPOD\pyMAPOD.pyc";  // 调用 Python 二进制文件
+            string sArguments = @"PythonScripts\pyMAPOD\pyMAPOD.py";  // 调用 Python 脚本
+            RunPythonScript(sArguments);
+        }
+    }
+}
